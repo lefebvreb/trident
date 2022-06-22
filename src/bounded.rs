3f3ff52cee5c7af058631397d65fc02410acb721
Benjamin Lefebvre <benji.lef76@gmail.com>
2022-06-23 01:13:08 +0200

    FormalParameterList

--- src/bounded.rs
+++ src/bounded.rs
@@ -10,7 +10,13 @@ pub enum Parameter<'id> {
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct FormalParameter<'id> {
-    n: u32,
+    i: u32,
+    id: Id<'id>,
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct FormalParameterList<'id> {
+    range: Range<u32>,
     id: Id<'id>,
 }
 
@@ -21,23 +27,48 @@ impl<'id> From<f64> for Parameter<'id> {
     }
 }
 
-impl<'id> Parameter<'id> {
+impl<'id> From<FormalParameter<'id>> for Parameter<'id> {
     #[inline]
-    pub(crate) fn formal(n: u32) -> Self {
-        Parameter::Formal(FormalParameter { n, id: Id::default() })
+    fn from(formal: FormalParameter<'id>) -> Self {
+        Parameter::Formal(formal)
     }
 }
 
-pub struct FormalParameterList<'id> {
-    range: Range<u32>,
-    id: Id<'id>,
+impl<'id> FormalParameter<'id> {
+    #[inline]
+    pub(crate) fn new(i: u32) -> FormalParameter<'id> {
+        FormalParameter { i, id: Id::default() }
+    } 
 }
 
 impl<'id> FormalParameterList<'id> {
-    
+    #[inline]
+    pub(crate) fn new(range: Range<u32>) -> Self {
+        Self { range, id: Id::default() }
+    }
+
+    #[inline]
+    pub fn len(&self) -> u32 {
+        self.range.end - self.range.start
+    }
+
+    #[inline]
+    pub fn get(&self, i: u32) -> Option<FormalParameter<'id>> {
+        (i < self.len()).then(|| FormalParameter::new(i + self.range.start))
+    }
+
+    #[inline]
+    pub fn contains(&self, parameter: FormalParameter<'id>) -> bool {
+        self.range.contains(&parameter.i)
+    }
+
+    #[inline]
+    pub fn iter(&self) -> impl Iterator<Item = FormalParameter<'id>> {
+        self.range.clone().map(|i| FormalParameter { i, id: Id::default() })
+    }
 }
 
 #[test]
 fn quick() {
-    eprintln!("{}", std::mem::size_of::<Parameter>());
+    eprintln!("{}", std::mem::size_of::<Option<f64>>());
 }
